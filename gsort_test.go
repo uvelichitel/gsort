fa6cef93c3a25b4163613a61b949e083bd2a2e84
kostarevilya <kostarevilya@yandex-team.ru>
2022-08-18 14:05:28 +0300

    Benchmark for []struct

--- gsort_test.go
+++ gsort_test.go
@@ -22,29 +22,60 @@ func (i ISl) Swap(x, y int) {
 	i[x], i[y] = i[y], i[x]
 }
 
-var slice = rand.Perm(100000)
-var islice = ISl(slice)
-
 func BenchmarkGenericSort(b *testing.B) {
+	b.StopTimer()
+	slice := rand.Perm(100000)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		GSort(slice, GLess)
 	}
 }
 
 func BenchmarkInterfaceSort(b *testing.B) {
+	b.StopTimer()
+	slice := rand.Perm(100000)
+	islice := ISl(slice)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		sort.Sort(islice)
 	}
 }
 
 func BenchmarkSliceSort(b *testing.B) {
+	b.StopTimer()
+	slice := rand.Perm(100000)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		sort.Slice(slice, func(i, j int) bool { return slice[i] < slice[j] })
 	}
 }
 
 func BenchmarkConcreteSort(b *testing.B) {
+	b.StopTimer()
+	slice := rand.Perm(100000)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		ISort(slice, GLess)
+	}
+}
+
+type strct struct {
+	p *int
+}
+
+func GStrctLess(a, b strct) bool {
+	return *(a.p) < *(b.p)
+}
+
+func BenchmarkStrctGenericSort(b *testing.B) {
+	b.StopTimer()
+	slice := rand.Perm(100000)
+	slStrct := make([]strct, 100000, 100000)
+	for i := range slice {
+		slStrct[i].p = &slice[i]
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		GSort(slStrct, GStrctLess)
 	}
 }
